5da7b5eeb410f1f8969e2bd58e77e15ada30eda6
Matt Watson <watson@tojourn.uk>
2022-08-19 16:07:10 +0100

    Add init files

---
+++ src/main/java/uk/tojourn/trophydatafinder/TrophyDataFinderApplication.java
@@ -0,0 +1,13 @@
+package uk.tojourn.trophydatafinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrophyDataFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrophyDataFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/tojourn/trophydatafinder/TrophyDataFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.tojourn.trophydatafinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrophyDataFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
